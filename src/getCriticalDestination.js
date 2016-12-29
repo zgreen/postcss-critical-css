f118e0932bfcd7b8c21bd3b3f145844c0430fec1
Zach Green <zach@alleyinteractive.com>
2016-12-29 18:59:35 -0500

    fix flow type typos

--- src/getCriticalDestination.js
+++ src/getCriticalDestination.js
@@ -7,7 +7,7 @@
  * @param {string} Default output CSS file name.
  * @return {string} String corresponding to output destination.
  */
-export function getCriticalDestination (rule: Object, dest: String): string {
+export function getCriticalDestination (rule: Object, dest: string): string {
   rule.walkDecls('critical-filename', (decl: Object) => {
     dest = decl.value.replace(/['"]*/g, '')
     decl.remove()
