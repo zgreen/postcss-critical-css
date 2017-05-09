b17dc57a242957f4c54fb665b44ed5f0f160ee1f
Zach Green <zhgreen@gmail.com>
2017-05-09 08:09:13 -0400

    catch errors and exit in preTest Promise

--- test/preTest.js
+++ test/preTest.js
@@ -8,22 +8,31 @@ function cb (files) {
   function useFileData (data, file) {
     postcss([postcssCriticalCSS({outputPath: basePath})])
       .process(data)
-      .then(result => fs.writeFile(
-        `${basePath}/${file.split('.')[0]}.non-critical.actual.css`,
-        result.css,
-        'utf8',
-        (err) => {
-          if (err) {
-            throw new Error(err)
+      .catch(err => {
+        console.error(err)
+        process.exit(1)
+      })
+      .then(result =>
+        fs.writeFile(
+          `${basePath}/${file.split('.')[0]}.non-critical.actual.css`,
+          result.css,
+          'utf8',
+          err => {
+            if (err) {
+              throw new Error(err)
+            }
           }
-        }))
+        )
+      )
   }
   files.forEach(function (file) {
     // Ignore any critical.css file(s) already written
     if (file !== 'critical.css') {
       if (file.indexOf('.actual') !== -1) {
-        fs.unlink(`${basePath}/${file}`, (err) => {
-          if (err) { throw new Error(err) }
+        fs.unlink(`${basePath}/${file}`, err => {
+          if (err) {
+            throw new Error(err)
+          }
         })
       }
       if (file.indexOf('.expected') === -1 && file.indexOf('.actual') === -1) {
